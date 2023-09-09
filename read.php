b045638aa3aae401aa53d2aa65f195163b34ed8b
saki komai <55061969+saki789@users.noreply.github.com>
2023-09-10 09:37:53 +1200

    Update read.php

--- read.php
+++ read.php
@@ -1,1 +1,33 @@
+<?php
+// Include your database configuration file here
 
+// Perform database retrieval
+// Modify this part to fetch data from your specific database table
+$sql = "SELECT * FROM your_table_name";
+$result = mysqli_query($conn, $sql);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Read</title>
+</head>
+<body>
+    <h2>Read Records</h2>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+        </tr>
+        <?php
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr>";
+            echo "<td>" . $row["name"] . "</td>";
+            echo "<td>" . $row["email"] . "</td>";
+            echo "</tr>";
+        }
+        mysqli_close($conn);
+        ?>
+    </table>
+</body>
+</html>
