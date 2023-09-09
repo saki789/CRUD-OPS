4c14fe81f4ee676f58577b87c083916cb712e18f
saki komai <55061969+saki789@users.noreply.github.com>
2023-09-10 09:39:04 +1200

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,1 +1,31 @@
+<?php
+// Include your database configuration file here
 
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+    
+    // Perform database deletion
+    // Modify this part to delete data from your specific database table
+    $sql = "DELETE FROM your_table_name WHERE id='$id'";
+    
+    if (mysqli_query($conn, $sql)) {
+        echo "Record deleted successfully.";
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
+    
+    mysqli_close($conn);
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Delete</title>
+</head>
+<body>
+    <h2>Delete Record</h2>
+    <p>Are you sure you want to delete this record?</p>
+    <a href="read.php">Cancel</a>
+    <a href="delete.php?id=<?php echo $_GET['id']; ?>">Confirm</a>
+</body>
+</html>
