9acbc12567cc068272b0ef405df74ac80b03ef47
saki komai <55061969+saki789@users.noreply.github.com>
2023-09-10 16:34:47 +1200

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -4,7 +4,6 @@ CREATE TABLE users (
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     full_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
     registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     privileged_type ENUM('Admin', 'Moderator', 'User') DEFAULT 'User'
 );
