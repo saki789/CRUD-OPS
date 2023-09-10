24f17f0b5908a7e2e31a1fe018269a753716bf86
saki komai <55061969+saki789@users.noreply.github.com>
2023-09-10 22:09:16 +1200

    Update update.php

--- update.php
+++ update.php
@@ -1,40 +1,46 @@
 <?php
-// Include your database configuration file here
+// Include the database connection
+include("db.php");
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get data from the form
-    $id = $_POST["id"];
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    
-    // Perform database update
-    // Modify this part to update data in your specific database table
-    $sql = "UPDATE your_table_name SET name='$name', email='$email' WHERE id='$id'";
-    
-    if (mysqli_query($conn, $sql)) {
+// Initialize variables for update
+$updateId = $date = $site = $guard = $inTime = $outTime = $status = $total = $extra = $grandTotal = $comments = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['update'])) {
+    $updateId = $_POST['editId'];
+    $date = $_POST['date'];
+    $site = $_POST['site'];
+    $guard = $_POST['guard'];
+    $inTime = $_POST['in_time'];
+    $outTime = $_POST['out_time'];
+    $status = $_POST['status'];
+    $total = $_POST['total'];
+    $extra = $_POST['extra'];
+    $grandTotal = $_POST['grand_total'];
+    $comments = $_POST['comments'];
+
+    // Prepare an SQL statement to update the record
+    $updateSql = "UPDATE duty_roster
+                  SET date = '$date',
+                      site = '$site',
+                      guard = '$guard',
+                      `IN TIME` = '$inTime',
+                      `OUT TIME` = '$outTime',
+                      status = '$status',
+                      total = '$total',
+                      extra = '$extra',
+                      grand_total = '$grandTotal',
+                      comments = '$comments'
+                  WHERE id = $updateId";
+
+    if ($conn->query($updateSql) === TRUE) {
         echo "Record updated successfully.";
     } else {
-        echo "Error: " . mysqli_error($conn);
+        echo "Error updating record: " . $conn->error;
     }
-    
-    mysqli_close($conn);
+} else {
+    echo "Invalid update request.";
 }
-?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Update</title>
-</head>
-<body>
-    <h2>Update Record</h2>
-    <form method="post" action="update.php">
-        <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>">
-        <label>Name:</label>
-        <input type="text" name="name" required><br>
-        <label>Email:</label>
-        <input type="email" name="email" required><br>
-        <input type="submit" value="Update">
-    </form>
-</body>
-</html>
+// Close the database connection
+$conn->close();
+?>
