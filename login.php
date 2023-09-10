615ca90a4bee8f5083dd589a9c2ca7f354d680b0
saki komai <55061969+saki789@users.noreply.github.com>
2023-09-10 17:30:56 +1200

    Update login.php

--- login.php
+++ login.php
@@ -1,20 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body>
-    <h1>Login</h1>
-    <form action="login.php" method="POST">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required><br><br>
+<?php
+// Include the database connection
+include("db.php");
 
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required><br><br>
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
-        <input type="submit" value="Login">
-    </form>
-</body>
-</html>
+    // Prepare a SQL statement with placeholders
+    $sql = "SELECT * FROM users WHERE username = ? LIMIT 1";
+    $stmt = mysqli_prepare($conn, $sql);
+    
+    if ($stmt) {
+        // Bind parameters and execute the statement
+        mysqli_stmt_bind_param($stmt, "s", $username);
+        mysqli_stmt_execute($stmt);
+
+        // Get the result
+        $result = mysqli_stmt_get_result($stmt);
+
+        // Check if a row was returned
+        if ($row = mysqli_fetch_assoc($result)) {
+            // Verify the hashed password
+            if (password_verify($password, $row['password'])) {
+                // Login successful, start a session
+                session_start();
+                $_SESSION["username"] = $username;
+                header("Location: dashboard.php");
+                exit();
+            } else {
+                echo "Login failed. Please check your username and password.";
+            }
+        } else {
+            echo "Login failed. Please check your username and password.";
+        }
+
+        // Close the statement
+        mysqli_stmt_close($stmt);
+    } else {
+        // Handle database error gracefully
+        echo "Database error: " . mysqli_error($conn);
+    }
+}
+
+// Close the database connection when done (if needed)
+// mysqli_close($conn);
+?>
