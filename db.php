64bf7841cf5573e628bfe5b962bbca60e78b1e27
saki komai <55061969+saki789@users.noreply.github.com>
2023-09-10 15:31:50 +1200

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,21 @@
 <?php
-$host = "localhost";
-$username = "your_username";
-$password = "your_password";
-$database = "your_database";
+// Database configuration
+$servername = "localhost"; // Change this to your MySQL server hostname
+$username = "your_username"; // Change this to your MySQL username
+$password = "your_password"; // Change this to your MySQL password
+$database = "your_database"; // Change this to your MySQL database name
 
-$conn = new mysqli($host, $username, $password, $database);
+// Create a connection to the MySQL database
+$conn = mysqli_connect($servername, $username, $password, $database);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Check the connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
+
+// Set character set to utf8 (if needed)
+mysqli_set_charset($conn, "utf8");
+
+// Close the database connection when done (if needed)
+// mysqli_close($conn);
 ?>
